b23f574f33181a5279a785eda94a20623337f6df
luisgepeto <luisgepeto@gmail.com>
2016-08-27 16:42:56 -0500

    primitive obsession annotation

--- Refactoring/Driver.cs
+++ Refactoring/Driver.cs
@@ -8,6 +8,7 @@ namespace Refactoring
        private string LicenseNumber { get; set; }
        private DateTime LicenseExpireDate { get; set; }
        private DateTime DateOfBirth { get; set; }
+        //primitive obsession
        public string AddressLine1 { get; set; }
        public string AddressLine2 { get; set; }
        public string City { get; set; }
@@ -42,16 +43,18 @@ namespace Refactoring
             if (DateOfBirth > today.AddYears(-age)) age--;
             return age;
         }
-        
+
         public string FormattedAddress()
         {
-            if (IsFullAddressEmpty(GetFormattedZip())) return String.Empty;
-            return FormatAddress(GetFormattedZip());
-        }
-
-        private string FormatAddress(string formattedZip)
-        {
-            var outAddress = AddressLine1;
+            var formattedZip = Zip;
+            if (Zip != null && Zip.Length > 5)
+            {
+                formattedZip= Zip.Substring(0, 5);
+            }
+            var fullAddress = String.Format("{0} {1} {2} {3} {4}", AddressLine1, AddressLine2, City, State, formattedZip).Trim();
+            var outAddress = String.Empty;
+            if (String.IsNullOrWhiteSpace(fullAddress)) return outAddress;
+            outAddress = AddressLine1;
             if (!String.IsNullOrWhiteSpace(AddressLine2)) outAddress += "\n" + AddressLine2;
             if (!String.IsNullOrWhiteSpace(City) || !String.IsNullOrWhiteSpace(State))
             {
@@ -62,22 +65,6 @@ namespace Refactoring
             }
             if (!String.IsNullOrWhiteSpace(formattedZip)) outAddress += "\n" + formattedZip;
             return outAddress;
-        }
-
-        private bool IsFullAddressEmpty(string formattedZip)
-        {
-            var fullAddress = String.Format("{0} {1} {2} {3} {4}", AddressLine1, AddressLine2, City, State, formattedZip).Trim();
-            return String.IsNullOrWhiteSpace(fullAddress);
-        }
-
-        private string GetFormattedZip()
-        {
-            var formattedZip = Zip;
-            if (Zip != null && Zip.Length > 5)
-            {
-                formattedZip = Zip.Substring(0, 5);
-            }
-            return formattedZip;
         }
     }
 }
