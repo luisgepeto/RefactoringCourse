654b3c6dfe5f286be35f43764b8551585a70d891
luisgepeto <luisgepeto@gmail.com>
2016-08-28 17:34:55 -0500

    encapsulated fields of properties

--- Refactoring/Customer.cs
+++ Refactoring/Customer.cs
@@ -4,14 +4,38 @@ namespace Refactoring
 {
     public class Customer
     {
-        //12 data class
+        private string _firstName;
+        private string _lastName;
+        private string _title;
+        private List<Account> _personalAccounts;
+
         public Customer()
         {
             PersonalAccounts = new List<Account>();
         }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Title { get; set; }
-        public List<Account> PersonalAccounts { get; set; }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public List<Account> PersonalAccounts
+        {
+            get { return _personalAccounts; }
+            set { _personalAccounts = value; }
+        }
     }
 }
